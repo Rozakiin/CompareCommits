45099e4520a7fa5633a4dd62e6531585d174938e
Rozakiin <ruari.armstrong@gmail.com>
2020-06-29 18:44:53 +0100

    Add SimilarityMatrix constructor tests

--- tests/CompareCommitsTests.cpp
+++ tests/CompareCommitsTests.cpp
@@ -1,5 +1,45 @@
 #include "gtest/gtest.h"
+#include "CompareCommits/CompareCommits.hpp"
 
 TEST(CompareCommitsTests, DemonstrateGTestMacros) {
     EXPECT_TRUE(true);
+}
+
+
+
+
+// Tests SimilarityMatrix constructed with size 0
+TEST(SimilarityMatrixConstructorTests, HandlesZeroSize) {
+    try {
+        SimilarityMatrix m = SimilarityMatrix(0);
+        FAIL() << "Expected: Constructor called with zero size";
+    }
+    catch(std::invalid_argument & e){
+        EXPECT_EQ(e.what(), std::string("Constructor called with zero size"));
+    }
+    catch(...){
+        FAIL() << "Expected: Constructor called with zero size";
+    }
+}
+
+// Tests SimilarityMatrix constructed with negative size - not sure if correct handling
+TEST(SimilarityMatrixConstructorTests, HandlesNegativeSize) {
+    SimilarityMatrix m = SimilarityMatrix(-1);
+    EXPECT_EQ(m.getCols(), 4294967295);
+}
+
+// Tests SimilarityMatrix constructor with positive size
+TEST(SimilarityMatrixConstructorTests, HandlesPositiveSize) {
+    {
+    SimilarityMatrix m = SimilarityMatrix(1);
+    EXPECT_EQ(m.getCols(), 1);
+    }
+    {
+    SimilarityMatrix m = SimilarityMatrix(2);
+    EXPECT_EQ(m.getCols(), 2);
+    }
+    {
+    SimilarityMatrix m = SimilarityMatrix(4294967295);
+    EXPECT_EQ(m.getCols(), 4294967295);
+    }
 }
